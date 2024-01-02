5213cec24bb836918fc4f9a1a1c16e06272cad89
Daniel Benita <danielb124@Daniels-MacBook-Air.local>
2024-01-02 12:25:09 +0200

    test

--- Hello_World/src/Hello_World/Hello.java
+++ Hello_World/src/Hello_World/Hello.java
@@ -1,6 +1,14 @@
 package Hello_World;
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Made it");
+        int x = 4;
+        double y = 7.3 ;
+
+        if(x>y){
+            System.out.println("x is bigger then y");
+        }
+        else{
+            System.out.println("Y is bigger then X");
+        }
     }
 }
